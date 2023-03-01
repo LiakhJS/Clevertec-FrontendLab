a1355db0c250adff1b608f7787b02038c0c39c8a
LiakhJS <anastasialiakh@hotmail.com>
2023-03-01 20:19:15 +0300

    btnStyles

--- src/components/registration/registration.jsx
+++ src/components/registration/registration.jsx
@@ -1,5 +1,5 @@
 // import { ReactComponent as EnterIcon } from '../../images/Enter_icon_arrow.svg';
-import { Fragment,  useState } from 'react';
+import { Fragment, useState } from 'react';
 // import { } from 'react';
 // import {  } from 'react';
 // import { } from 'react';
@@ -13,20 +13,36 @@ import { CustomInput } from '../custom-input';
 import { InputId, InputMessage, InputPlaceholder, InputType } from '../utils.js';
 
 import './registration.css';
+import '../button/button.css';
 
 export const Registration = () => {
-  const {
-    register,
-    handleSubmit,
-      watch,
-    formState: { errors },
-  } = useForm({});
-  const onSubmit = (data) => console.log(data);
-
+  const [step, setStep] = useState(1);
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const btnTitle = step === 1 ? 'Следующий шаг' : step === 2 ? 'Последний шаг' : 'Зарегистрироваться';
+  const setHandleStep = () => {
+    setStep(step + 1);
+  };
   const setPassword = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({});
+  const onSubmit = (data) => {
+    if (step < 3) {
+      // btnStyle = 'reserv-btn reserved';
+
+      setHandleStep();
+    } else {
+      // btnStyle = 'reserv-btn';
+      console.log(data);
+    }
+  };
+  const btnStyle = (step < 3 && !!errors.login) || !!errors.password ? 'reserv-btn ordered' : 'reserv-btn';
 
   return (
     <div className='container'>
@@ -34,51 +50,110 @@ export const Registration = () => {
       <form className='custom-form' noValidate={true} onSubmit={handleSubmit(onSubmit)}>
         <h4 className='custom-title'>Регистрация</h4>
         <div className='custom-step'>
-          <span>1</span> шаг из 3
+          <span>{step}</span> шаг из 3
         </div>
+        {step === 1 && (
+          <Fragment>
+            <CustomInput
+              type={InputType.Text}
+              id={InputId.Login}
+              placehold={InputPlaceholder.CreateLogin}
+              message={InputMessage.CreateUserName}
+              errors={errors.login}
+              required={true}
+              autoComplete='on'
+              {...register('login', {
+                required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
+              })}
+            />
 
-        <CustomInput
-          type={InputType.Text}
-          id={InputId.Login}
-          placehold={InputPlaceholder.CreateLogin}
-          message={InputMessage.CreateUserName}
-          errors={errors.login}
-          required={true}
-          autoComplete='on'
-          {...register('login', {
-            required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
-          })}
-        />
+            <div className='custom-item'>
+              <input
+                id={InputId.Password}
+                autoComplete='on'
+                type={isPasswordVisible ? InputType.Text : InputType.Password}
+                className='custom-input'
+                required={true}
+                {...register('password', { required: { value: true, minlength: 5 } })}
+              />
+              <label htmlFor='password' className='custom-label'>
+                {InputPlaceholder.Password}
+              </label>
+              {isPasswordVisible ? (
+                <Fragment>
+                  {!errors.password && watch('password') && <CheckedIcon className='done' />}
+                  <EyeIsOpened onClick={setPassword} className='eye' />
+                </Fragment>
+              ) : (
+                <Fragment>
+                  {!errors.password && watch('password') && <CheckedIcon className='done' />}
+                  <EyeIsClosed onClick={setPassword} className='eye' />
+                </Fragment>
+              )}
+              {errors.password && <span className='field_invalid'>{InputMessage.Password}</span>}
+              {!errors.password && <span className='field_valid'>{InputMessage.Password}</span>}
+            </div>
+          </Fragment>
+        )}
 
-        <div className='custom-item'>
-          <input
-    
-            id={InputId.Password}
-            autoComplete='on'
-            type={isPasswordVisible ? InputType.Text : InputType.Password}
-            className='custom-input'
-            required={true}
-            {...register('password', { required: { value: true, minlength: 5 } })}
-          />
-          <label htmlFor='password' className='custom-label'>
-            {InputPlaceholder.Password}
-          </label>
-          {isPasswordVisible ? (
-            <Fragment>
-              {!errors.password  &&  watch('password') && <CheckedIcon className='done' />}
-              <EyeIsOpened onClick={setPassword} className='eye' />
-            </Fragment>
-          ) : (
-            <Fragment>
-              {!errors.password  &&  watch('password') && <CheckedIcon className='done' />}
-              <EyeIsClosed onClick={setPassword} className='eye' />
-            </Fragment>
-          )}
-          {errors.password && <span className='field_invalid'>{InputMessage.Password}</span>}
-          {!errors.password && <span className='field_valid'>{InputMessage.Password}</span>}
-        </div>
+        {step === 2 && (
+          <Fragment>
+            <CustomInput
+              type={InputType.Text}
+              id={InputId.Login}
+              placehold={InputPlaceholder.CreateLogin}
+              message={InputMessage.CreateUserName}
+              errors={errors.login}
+              required={true}
+              autoComplete='on'
+              {...register('login', {
+                required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
+              })}
+            />
+            <CustomInput
+              type={InputType.Text}
+              id={InputId.Login}
+              placehold={InputPlaceholder.CreateLogin}
+              message={InputMessage.CreateUserName}
+              errors={errors.login}
+              required={true}
+              autoComplete='on'
+              {...register('login', {
+                required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
+              })}
+            />
+          </Fragment>
+        )}
+        {step === 3 && (
+          <Fragment>
+            <CustomInput
+              type={InputType.Text}
+              id={InputId.Login}
+              placehold={InputPlaceholder.CreateLogin}
+              message={InputMessage.CreateUserName}
+              errors={errors.login}
+              required={true}
+              autoComplete='on'
+              {...register('login', {
+                required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
+              })}
+            />
+            <CustomInput
+              type={InputType.Text}
+              id={InputId.Login}
+              placehold={InputPlaceholder.CreateLogin}
+              message={InputMessage.CreateUserName}
+              errors={errors.login}
+              required={true}
+              autoComplete='on'
+              {...register('login', {
+                required: { value: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ },
+              })}
+            />
+          </Fragment>
+        )}
 
-        <input type='submit' />
+        <input type='submit' value={btnTitle} className={btnStyle} />
       </form>
     </div>
   );
